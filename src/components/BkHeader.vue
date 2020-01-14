a878108cf23a4c55406b01a91c67378c4db2ab5a
Briam <briammartinez@gmail.com>
2020-01-14 01:17:10 +0100

    File structure added

---
+++ src/components/BkHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <img
+      class="logo"
+      src="../assets/logo.png"
+      alt="logo"
+    />
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'BkHeader'
+}
+</script>
+
+<style scoped>
+  .logo {
+    height: 60px;
+    width: 60px;
+  }
+</style>
