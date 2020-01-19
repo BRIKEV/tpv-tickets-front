b5aa6bb4cdfb0ee3a0c495b4f1500f3da8c1072f
Briam <briammartinez@gmail.com>
2020-01-19 18:43:23 +0100

    Added Login route

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,52 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Admin from '@/views/Admin';
+import Login from '@/views/Login';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
 
   routes: [
     {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      meta: {
+        public: true,
+        onlyWhenLoggedOut: true
+      }
+    },
+    {
       path: '/',
       name: 'admin',
       component: Admin,
+      meta: {
+        public: true,
+        onlyWhenLoggedOut: true
+      }
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  const isPublic = to.matched.some(record => record.meta.public)
+  const onlyWhenLoggedOut = to.matched.some(record => record.meta.onlyWhenLoggedOut)
+  const loggedIn = !!undefined;
+
+  if (!isPublic && !loggedIn) {
+    return next({
+      path:'/login',
+      // query: {redirect: to.fullPath}  // Store the full path to redirect the user to after login
+    });
+  }
+
+  if (loggedIn && onlyWhenLoggedOut) {
+    return next('/');
+  }
+
+  next();
+})
+
+export default router;
