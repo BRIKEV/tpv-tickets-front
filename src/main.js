79429fc94f06e7fd5677330e606cf3b134ac236c
Briam Martinez <briammartinez@gmail.com>
2020-01-28 22:24:54 +0100

    Added theme

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import i18n from '@/lang/i18n';
 import './registerServiceWorker';
 import router from '@/router/routes';
-import './theme/index.scss';
+import '@/theme/index.scss';
 
 Vue.config.productionTip = false;
 
