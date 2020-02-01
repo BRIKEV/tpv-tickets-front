bb24baa6cf3f12bf9e03b2fe47dcfa216d9dcd80
Briam Martinez <briammartinez@gmail.com>
2020-02-01 18:27:09 +0100

    Created BkInput component

---
+++ src/components/BkInput/BkInput.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="input-container">
+    <input
+      :value="value"
+      :name="name"
+      :type="type"
+      required
+      @input="$emit('input', $event.target.value)"
+    />
+    <label>{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BkInput',
+  props: {
+    value: String,
+    name: String,
+    type: String,
+    label: String,
+    required: Boolean,
+  },
+};
+</script>
+<style lang="scss" scoped>
+  @import "@/theme/index.scss";
+  .input-container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    position: relative;
+    margin: 30px 0;
+    input {
+      color: $white;
+      font-size: $fs-small;
+      border: none;
+      width: 100%;
+      outline: none;
+      background: none;
+      height: 50px;
+      border-bottom: 2px solid $white;
+      &:active ~label,
+      &:focus ~label,
+      &:valid ~label {
+        bottom: 50px;
+      }
+    }
+    label {
+      color: $white;
+      bottom: 12px;
+      position: absolute;
+      font-size: $fs-large;
+      font-weight: 700;
+      transition: all .3s ease;
+    }
+  }
+</style>
