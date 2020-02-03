0a3818443f49dc4d908b674166b99b84985e2edf
Briam Martinez <briammartinez@gmail.com>
2020-02-03 20:38:19 +0100

    Created notification store with its actions

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import tickets from './tickets/ticketsStore';
+import notification from './notification/notificationsStore';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
     tickets,
+    notification,
   },
 });
 
