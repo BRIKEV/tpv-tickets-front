8b71e7355f82b5d30b9f06c44dbe2060e282cce4
Briam Martinez <briammartinez@gmail.com>
2020-02-03 20:43:05 +0100

    Added action of updateTicket

--- src/store/tickets/ticketsActions.js
+++ src/store/tickets/ticketsActions.js
@@ -1,8 +1,14 @@
-import { getTickets } from '@/api';
+/* eslint-disable no-unused-vars */
+import { getTickets, register } from '@/api';
 
 export default {
   getTickets({ commit }) {
     return getTickets()
       .then(({ data }) => commit('SET_ITEMS', data));
+  },
+  updateTicket({ commit, dispatch }, payload) {
+    return register(payload)
+      .then(() => dispatch('sendSucces', { title: 'SUCCESS', text: 'Ticket check successfully' }))
+      .catch(() => dispatch('sendError', { title: 'ERROR', text: 'The ticket could not be checked' }));
   },
 };
