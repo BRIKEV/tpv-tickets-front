a12b734fdd47bbd5fbfb731c0a058d033c5c369c
Briam Martinez <briammartinez@gmail.com>
2020-02-04 23:12:54 +0100

    Added loading store to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex';
 
 import tickets from './tickets/ticketsStore';
 import notification from './notification/notificationsStore';
+import loading from './loading/loadingStore';
 
 Vue.use(Vuex);
 
@@ -10,6 +11,7 @@ const store = new Vuex.Store({
   modules: {
     tickets,
     notification,
+    loading,
   },
 });
 
