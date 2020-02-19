33ed0ba12b1613d502d3a86a911d6b910692b985
Briam Martinez <briammartinez@gmail.com>
2020-02-19 22:23:23 +0100

    Added transition and fix position of BkCollapse component

--- src/components/BkCollapse/BkCollapse.vue
+++ src/components/BkCollapse/BkCollapse.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="container" :class="{ 'not-expanded': !this.expanded }">
-      <div class="header" @click="toggleCardState">
-        <p class="title" name="header">{{ title }}</p>
-        <i class="material-icons icon">keyboard_arrow_down</i>
+  <div
+    class="container"
+    :class="{ 'not-expanded': !this.expanded }">
+      <div
+        data-cy="toggle-collapse"
+        class="header"
+        @click="toggleCardState">
+          <p class="title" name="header">{{ title }}</p>
+          <i class="material-icons icon">keyboard_arrow_down</i>
       </div>
-      <div class="content" v-if="expanded">
-          <slot></slot>
-      </div>
+      <transition
+        name="fade"
+        mode="out-in">
+        <div
+          class="content"
+          v-if="expanded">
+            <slot></slot>
+        </div>
+      </transition>
   </div>
 </template>
 
@@ -36,11 +47,17 @@ export default {
 <style lang="scss" scoped>
 @import "@/theme/index.scss";
 $shadow: -1px 0px 7px -4px rgba(0,0,0,0.98);
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
 .container {
   position: relative;
   box-shadow: $shadow;
   padding: 15px;
-  height: 30px;
+  height: 100%;
   background: $white;
 }
 .header {
@@ -60,10 +77,8 @@ $shadow: -1px 0px 7px -4px rgba(0,0,0,0.98);
   }
 }
 .content {
-  position: absolute;
-  box-shadow: $shadow;
-  padding: 10px;
-  min-height: auto;
-  overflow: hidden;
+  position: relative;
+  margin-top: calculateRem(15px);
+
 }
 </style>
