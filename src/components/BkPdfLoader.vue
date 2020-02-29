101c6135da965f2bde416412f1850de229c64841
Briam Martinez <briammartinez@gmail.com>
2020-02-29 12:11:35 +0100

    renamed and uncoupled the component BkPdfLoader

---
+++ src/components/BkPdfLoader.vue
@@ -0,0 +1,103 @@
+<template>
+  <form novalidate @submit.prevent="sendFile">
+    <div class="pdf-container">
+      <label for="file">
+        <span v-if="!selectedName">{{ $t('pdf.fileInput') }}</span>
+        <span v-else>{{ selectedName }}</span>
+      </label>
+      <input
+        accept="application/pdf"
+        name="file"
+        id="file"
+        required
+        v-on:change="setFile"
+        type="file"
+      />
+    </div>
+    <BkButton
+      data-cy="btn"
+      class="btn"
+      type="submit"
+      :disabled="loading"
+      :isLoading="loading"
+    >
+        {{ $t('pdf.button') }}
+    </BkButton>
+  </form>
+</template>
+
+<script>
+
+export default {
+  name: 'BkPdfLoader',
+
+  data() {
+    return {
+      pdfFile: null,
+      selectedName: '',
+    };
+  },
+
+  props: {
+    loading: Boolean,
+  },
+
+  methods: {
+    setFile(evt) {
+      const [file] = evt.target.files;
+      this.selectedName = file.name;
+      this.pdfFile = file;
+    },
+    sendFile(evt) {
+      if (evt.target.checkValidity()) {
+        const data = new FormData();
+        data.append('file', this.pdfFile);
+        this.$emit('sendFile', data);
+      }
+      return null;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import "@/theme/index.scss";
+
+  $file-border-color: #000;
+
+  form {
+    width: 80%;
+    margin: 0 auto;
+  }
+  .pdf-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    height: 200px;
+    border: 2px dotted $file-border-color;
+    border-radius: 10px;
+    position: relative;
+    margin-top: 30px;
+
+    label {
+      width: 100%;
+      height: 100%;
+      cursor: pointer;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+
+    input {
+      position: absolute;
+      visibility: hidden;
+    }
+  }
+  .btn {
+    width: 100%;
+    margin: 20px 0;
+  }
+</style>
