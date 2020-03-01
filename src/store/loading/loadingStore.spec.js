40bda8e59927b7e65a3ce6589017dbf5021e6e8e
Briam Martinez <briammartinez@gmail.com>
2020-03-01 21:52:04 +0100

    Added test of tickets store

--- src/store/loading/loadingStore.spec.js
+++ src/store/loading/loadingStore.spec.js
@@ -1,9 +1,9 @@
 import { initialState, mutations } from './loadingStore';
 
-describe('Store/wizard/cargoes/mutations', () => {
+describe('Loading store', () => {
   const state = { ...initialState };
 
-  it('set ships correctly', () => {
+  it('set loading correctly', () => {
     expect(state.loading).toEqual(initialState.loading);
     mutations.SET_LOADING(state, true);
     expect(state.loading).toEqual(true);
