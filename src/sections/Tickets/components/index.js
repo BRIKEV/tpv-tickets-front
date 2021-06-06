0d4e61398143be00b4220da539a8d957513343ee
Briam Martinez <briammartinez@gmail.com>
2021-06-06 18:14:25 +0200

    naming refactor

--- src/sections/Tickets/components/index.js
+++ src/sections/Tickets/components/index.js
@@ -1,2 +1,2 @@
-export { default as MainModal } from './MainModal.vue';
-export { default as MainTable } from './MainTable.vue';
+export { default as MainModal } from './TicketModal.vue';
+export { default as MainTable } from './TicketTable.vue';
